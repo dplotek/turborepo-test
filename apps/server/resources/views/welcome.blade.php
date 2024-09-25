b6a7073a4b0fe9a007a15059a08ead8752b78a9e
dplotek <da.plotek@gmail.com>
2024-09-25 19:15:11 +0200

    test: server build

--- apps/server/resources/views/welcome.blade.php
+++ apps/server/resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel with</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
@@ -164,6 +164,7 @@
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
+                        <p>Turborepo</p>
                     </footer>
                 </div>
             </div>
