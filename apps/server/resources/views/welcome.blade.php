2a99e4726101229c9e9d2edb2152429010e1c820
dplotek <da.plotek@gmail.com>
2024-09-28 00:10:25 +0200

    test: deploy v11

--- apps/server/resources/views/welcome.blade.php
+++ apps/server/resources/views/welcome.blade.php
@@ -164,7 +164,7 @@
 
                     <footer class="py-16 text-center text-sm text-black dark:text-white/70">
                         Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
-                        <p>Turborepo v6</p>
+                        <p>Turborepo v11</p>
                     </footer>
                 </div>
             </div>
